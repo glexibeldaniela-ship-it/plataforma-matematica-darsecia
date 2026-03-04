72a7766b74407cf60f3c729868f29bde97ce75e5
glexibeldaniela-ship-it <glexibeldaniela@gmail.com>
2026-03-03 22:08:39 -0400

    Actualizar crear-examen.js

--- crear-examen.js
+++ crear-examen.js
@@ -1,28 +1,44 @@
 import { auth, db } from "./firebase.js";
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { collection, addDoc, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+let preguntas = [];
+
+window.agregarPregunta = function () {
+
+  const index = preguntas.length;
+
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <hr>
+    <select id="tipo_${index}">
+      <option value="opcion_multiple">Opción múltiple</option>
+      <option value="numerica">Numérica</option>
+      <option value="completar">Completar palabra</option>
+    </select><br><br>
+
+    <input type="text" id="enunciado_${index}" placeholder="Enunciado"><br><br>
+
+    <div id="extra_${index}"></div>
+  `;
+
+  document.getElementById("contenedorPreguntas").appendChild(div);
+
+  preguntas.push({});
+};
 
 document.getElementById("btnGuardar").addEventListener("click", async () => {
 
   const user = auth.currentUser;
+  if (!user) return alert("No autorizado");
 
-  if (!user) {
-    alert("No autorizado");
-    return;
-  }
-
-  const titulo = document.getElementById("titulo").value;
-  const anio = document.getElementById("anio").value;
-  const seccion = document.getElementById("seccion").value;
-  const lapso = document.getElementById("lapso").value;
+  const titulo = titulo.value;
+  const anio = anio.value;
+  const seccion = seccion.value;
+  const lapso = lapso.value;
   const duracion = parseInt(document.getElementById("duracion").value);
-  const repaso = document.getElementById("repaso").value;
+  const repaso = repaso.value;
 
-  if (!titulo || !anio || !seccion || !lapso || !duracion) {
-    alert("Complete todos los campos");
-    return;
-  }
-
-  await addDoc(collection(db, "examenes"), {
+  const examenRef = await addDoc(collection(db, "examenes"), {
     titulo,
     anio,
     seccion,
@@ -35,7 +51,20 @@ document.getElementById("btnGuardar").addEventListener("click", async () => {
     creado_en: new Date()
   });
 
-  alert("Examen creado correctamente");
+  for (let i = 0; i < preguntas.length; i++) {
+
+    const tipo = document.getElementById(`tipo_${i}`).value;
+    const enunciado = document.getElementById(`enunciado_${i}`).value;
+
+    await addDoc(collection(db, "examenes", examenRef.id, "preguntas"), {
+      tipo,
+      enunciado,
+      puntos: 1
+    });
+
+  }
+
+  alert("Examen con preguntas creado correctamente");
   window.location.href = "panel-profesor.html";
 
 });
