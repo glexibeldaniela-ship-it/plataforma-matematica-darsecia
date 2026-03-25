5efa776ad1853a07cb1cf7184607409d08756db2
glexibeldaniela-ship-it <glexibeldaniela@gmail.com>
2026-03-25 00:17:54 -0400

    Eliminar supabase.js

--- js/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-
-const supabaseUrl = 'https://dugwjmafpffeykpkkpna.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1Z3dqbWFmcGZmZXlrcGtrcG5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyMDE0NDIsImV4cCI6MjA4ODc3NzQ0Mn0.WS-DSZPMxnlV1HKTCTjwYMODVXGyjf7wB5MVJ5NklCs'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
